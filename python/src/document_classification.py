9a257295bef8ed477d0c1502776beb59155ec1e1
jasonravagli <jasonravagli@gmail.com>
2020-04-21 19:09:40 +0200

    Colab notebook to train VGG16 translated into python module.
    Python files created to make the project configurable and executable from command line (at the moment, is available only the training of the VGG16 net)
    
    training_vgg16.py needs to be cleaned and reformatted

---
+++ python/src/document_classification.py
@@ -0,0 +1,19 @@
+import os
+import argparse
+import logging
+import python.src.configuration as config
+
+if __name__ == "__main__":
+    # Parse arguments
+    parser = argparse.ArgumentParser(description='Document Classification')
+    parser.add_argument("--generate_dataset", type=bool, help="Whether to generate the dataset to be used from the CNN training process")
+    parser.add_argument("--train_network", type=bool, help="Whether to train the CNN or not")
+    parser.add_argument("--resume_training", type=bool, help="Whether to resum the trainining of the CNN from the last checkpoint")
+    args = parser.parse_args()
+
+    config.add_command_line_args(args)
+    config.set_application_root_path(os.path.abspath("../../"))
+
+    config.set_log_level(logging.DEBUG)
+
+    import python.src.training_vgg16
