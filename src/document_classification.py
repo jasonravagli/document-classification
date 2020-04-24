c460fff558c187053c3b0366838d17383d57f5b1
jasonravagli <jasonravagli@gmail.com>
2020-04-24 12:06:44 +0200

    Command line args for training configuration added.

--- src/document_classification.py
+++ src/document_classification.py
@@ -9,9 +9,12 @@ if __name__ == "__main__":
     parser.add_argument("--generate_dataset", action="store_true", help="Generate the dataset to be used from the CNN training process")
     parser.add_argument("--train_network", action="store_true", help="Train the CNN")
     parser.add_argument("--resume_training", action="store_true", help="Resume the trainining of the CNN from the last checkpoint")
+    parser.add_argument("--training.lr", dest="lr", type=float, help="Learning rate")
+    parser.add_argument("--training.batch_size", dest="batch_size", type=int, help="Batch size used for training")
     args = parser.parse_args()
 
     config.add_command_line_args(args)
+    config.config["training"].set_args(args)
     config.set_application_root_path(os.path.abspath("../"))
 
     config.configure_logger(logging.DEBUG)
--- src/training_vgg16.py
+++ src/training_vgg16.py
@@ -11,8 +11,6 @@ Original file is located at
 Install required packages:
 """
 
-
-
 """Import required packages:"""
 
 import os
@@ -33,12 +31,18 @@ import configuration as config
 
 """General parameters and settings:"""
 
-original_dataset_path = os.path.join(config.config["paths"]["root"].get(), config.config["paths"]["original_dataset"].get()) #"/content/drive/My Drive/document-classification/datasets/rvl-cdip/mini-dataset-1600-200-200.h5"
-cnn_dataset_path = os.path.join(config.config["paths"]["root"].get(), config.config["paths"]["cnn_dataset"].get()) #"/content/drive/My Drive/document-classification/datasets/rvl-cdip/cnn-mini-dataset-1600-200-200.h5"
-cnn_model_path = os.path.join(config.config["paths"]["root"].get(), config.config["paths"]["cnn_model"].get()) #"/content/drive/My Drive/document-classification/models/model.json"
-cnn_weights_path = os.path.join(config.config["paths"]["root"].get(), config.config["paths"]["cnn_weights"].get()) #"/content/drive/My Drive/document-classification/models/weights.h5"
-cnn_weights_checkpoint_path = os.path.join(config.config["paths"]["root"].get(), config.config["paths"]["cnn_weights_checkpoint"].get()) #"/content/drive/My Drive/document-classification/checkpoints/checkpoint_weights.h5"
-history_stats_path = os.path.join(config.config["paths"]["root"].get(), config.config["paths"]["history_stats"].get()) #"/content/drive/My Drive/document-classification/history/history_stats.csv"
+original_dataset_path = os.path.join(config.config["paths"]["root"].get(), config.config["paths"][
+    "original_dataset"].get())  # "/content/drive/My Drive/document-classification/datasets/rvl-cdip/mini-dataset-1600-200-200.h5"
+cnn_dataset_path = os.path.join(config.config["paths"]["root"].get(), config.config["paths"][
+    "cnn_dataset"].get())  # "/content/drive/My Drive/document-classification/datasets/rvl-cdip/cnn-mini-dataset-1600-200-200.h5"
+cnn_model_path = os.path.join(config.config["paths"]["root"].get(), config.config["paths"][
+    "cnn_model"].get())  # "/content/drive/My Drive/document-classification/models/model.json"
+cnn_weights_path = os.path.join(config.config["paths"]["root"].get(), config.config["paths"][
+    "cnn_weights"].get())  # "/content/drive/My Drive/document-classification/models/weights.h5"
+cnn_weights_checkpoint_path = os.path.join(config.config["paths"]["root"].get(), config.config["paths"][
+    "cnn_weights_checkpoint"].get())  # "/content/drive/My Drive/document-classification/checkpoints/checkpoint_weights.h5"
+history_stats_path = os.path.join(config.config["paths"]["root"].get(), config.config["paths"][
+    "history_stats"].get())  # "/content/drive/My Drive/document-classification/history/history_stats.csv"
 
 # Flag to generate CNN dataset (set it to False if the dataset has already been generated)
 generate_dataset = config.config["generate_dataset"].get(bool)
@@ -49,15 +53,17 @@ train_network = config.config["train_network"].get(bool)
 resume_training = config.config["resume_training"].get(bool)
 
 # Shape of the images read from the mini-dataset
-original_img_shape = config.config["original_img_shape"].get() #(1000, 750)
+original_img_shape = config.config["original_img_shape"].get()  # (1000, 750)
 # Shape of the images in input to the CNN
-cnn_image_shape = config.config["cnn_image_shape"].get() #(500, 375)
+cnn_image_shape = config.config["cnn_image_shape"].get()  # (500, 375)
 # Number of images classes
-n_classes = config.config["n_classes"].get(int) #16
+n_classes = config.config["n_classes"].get(int)  # 16
 
-print(generate_dataset)
-print(train_network)
-print(resume_training)
+learning_rate = config.config["training"]["lr"].get(float)
+batch_size = config.config["training"]["batch_size"].get(int)
+
+print(learning_rate)
+print(batch_size)
 
 """Check if GPU is available:"""
 
@@ -72,63 +78,63 @@ assert tf.test.is_gpu_available()
 It reads all images from the original dataset, preprocesses them according to the CNN requirements (resize, convert to RGB) and finally saves them into the new dataset.
 """
 
+
 def generate_dataset_for_cnn(hdf_original_file, hdf_cnn_file, dataset_name):
-  # Number of data rows to read from original file, process and write to the CNN file at the same time
-  # (To speedup read/write operations)
-  batch_size = 64
+    # Number of data rows to read from original file, process and write to the CNN file at the same time
+    # (To speedup read/write operations)
+    batch_size = 64
 
-  # Read datasets from the original HDF5 file
-  ds_original_imgs = hdf_original_file[dataset_name]
-  ds_original_labels = hdf_original_file[dataset_name + "_labels"]
+    # Read datasets from the original HDF5 file
+    ds_original_imgs = hdf_original_file[dataset_name]
+    ds_original_labels = hdf_original_file[dataset_name + "_labels"]
 
-  n_imgs = ds_original_imgs.len()
+    n_imgs = ds_original_imgs.len()
 
-  # Create omonymous datasets into the HDF5 file for the CNN
-  ds_cnn_imgs = hdf_cnn_file.create_dataset(dataset_name, (n_imgs, cnn_image_shape[0], cnn_image_shape[1], 3),\
-                                      dtype="float32", compression="gzip")
-  ds_cnn_labels = hdf_cnn_file.create_dataset(dataset_name + "_labels", (n_imgs, n_classes),\
-                                    dtype="int8", compression="gzip")
-  
-  n_batches = n_imgs//batch_size
-  for i in tqdm(range(n_batches)):
-    # Load data in batches
-    start_batch_index = i*batch_size
-    end_batch_index = np.min([(i+1)*batch_size, n_imgs])
-    original_batch_imgs = ds_original_imgs[start_batch_index:end_batch_index]
-    original_batch_labels = ds_original_labels[start_batch_index:end_batch_index]
+    # Create omonymous datasets into the HDF5 file for the CNN
+    ds_cnn_imgs = hdf_cnn_file.create_dataset(dataset_name, (n_imgs, cnn_image_shape[0], cnn_image_shape[1], 3), dtype="float32", compression="gzip")
+    ds_cnn_labels = hdf_cnn_file.create_dataset(dataset_name + "_labels", (n_imgs, n_classes), dtype="int8", compression="gzip")
 
-    cnn_batch_imgs = np.empty((batch_size, cnn_image_shape[0], cnn_image_shape[1], 3), dtype=np.float32)
-    # Labels do not need to be processed
-    cnn_batch_labels = original_batch_labels
+    n_batches = n_imgs // batch_size
+    for i in tqdm(range(n_batches)):
+        # Load data in batches
+        start_batch_index = i * batch_size
+        end_batch_index = np.min([(i + 1) * batch_size, n_imgs])
+        original_batch_imgs = ds_original_imgs[start_batch_index:end_batch_index]
+        original_batch_labels = ds_original_labels[start_batch_index:end_batch_index]
 
-    # Preprocess all images in batch
-    for i in range(batch_size):
-      original_img = original_batch_imgs[i]
-      # De-normalization: bring back pixel values to the [0,255] range
-      non_normalized_img = np.asarray(original_img * 255, dtype=np.uint8)
-      # Scale the image to the target shape (height/width ratio is preserved due
-      # to the original and target shape values)
-      scaled_img = cv2.resize(non_normalized_img, (cnn_image_shape[1], cnn_image_shape[0]),\
-                              interpolation=cv2.INTER_AREA)
-      # Convert grayscale image to RGB (VGG16 needs color images)
-      rgb_img = cv2.cvtColor(scaled_img, cv2.COLOR_GRAY2RGB)
-      # Normalization
-      cnn_batch_imgs[i] = rgb_img / 255
-    
-    # Write data batches
-    ds_cnn_imgs[start_batch_index:end_batch_index] = cnn_batch_imgs
-    ds_cnn_labels[start_batch_index:end_batch_index] = cnn_batch_labels
+        cnn_batch_imgs = np.empty((batch_size, cnn_image_shape[0], cnn_image_shape[1], 3), dtype=np.float32)
+        # Labels do not need to be processed
+        cnn_batch_labels = original_batch_labels
+
+        # Preprocess all images in batch
+        for i in range(batch_size):
+            original_img = original_batch_imgs[i]
+            # De-normalization: bring back pixel values to the [0,255] range
+            non_normalized_img = np.asarray(original_img * 255, dtype=np.uint8)
+            # Scale the image to the target shape (height/width ratio is preserved due
+            # to the original and target shape values)
+            scaled_img = cv2.resize(non_normalized_img, (cnn_image_shape[1], cnn_image_shape[0]), \
+                                    interpolation=cv2.INTER_AREA)
+            # Convert grayscale image to RGB (VGG16 needs color images)
+            rgb_img = cv2.cvtColor(scaled_img, cv2.COLOR_GRAY2RGB)
+            # Normalization
+            cnn_batch_imgs[i] = rgb_img / 255
+
+        # Write data batches
+        ds_cnn_imgs[start_batch_index:end_batch_index] = cnn_batch_imgs
+        ds_cnn_labels[start_batch_index:end_batch_index] = cnn_batch_labels
+
 
 """For each set (train. validation, test) in the original HDF5 dataset file, generate a set in a new HDF5 file to be used during the CNN training phase:"""
 
 if generate_dataset:
-  with h5py.File(original_dataset_path, 'r') as hdf_original_file, h5py.File(cnn_dataset_path, 'w') as hdf_cnn_file:
-    logging.info("Generating training dataset for CNN...")
-    generate_dataset_for_cnn(hdf_original_file, hdf_cnn_file, "train")
-    logging.info("Generating validation dataset for CNN...")
-    generate_dataset_for_cnn(hdf_original_file, hdf_cnn_file, "valid")
-    logging.info("Generating test dataset for CNN...")
-    generate_dataset_for_cnn(hdf_original_file, hdf_cnn_file, "test")
+    with h5py.File(original_dataset_path, 'r') as hdf_original_file, h5py.File(cnn_dataset_path, 'w') as hdf_cnn_file:
+        logging.info("Generating training dataset for CNN...")
+        generate_dataset_for_cnn(hdf_original_file, hdf_cnn_file, "train")
+        logging.info("Generating validation dataset for CNN...")
+        generate_dataset_for_cnn(hdf_original_file, hdf_cnn_file, "valid")
+        logging.info("Generating test dataset for CNN...")
+        generate_dataset_for_cnn(hdf_original_file, hdf_cnn_file, "test")
 
 """#Utility classes
 
@@ -137,9 +143,11 @@ if generate_dataset:
 Class to handle large datasets (see https://stanford.edu/~shervine/blog/keras-how-to-generate-data-on-the-fly for further details)
 """
 
+
 class DataGenerator(keras.utils.Sequence):
     'Generates data for Keras'
-    def __init__(self, ds_imgs, ds_labels, batch_size=32, dim=(32,32,32), n_channels=3,
+
+    def __init__(self, ds_imgs, ds_labels, batch_size=32, dim=(32, 32, 32), n_channels=3,
                  n_classes=10, shuffle=True):
         'Initialization'
         self.dim = dim
@@ -158,7 +166,7 @@ class DataGenerator(keras.utils.Sequence):
     def __getitem__(self, index):
         'Generate one batch of data'
         # Generate indexes of the batch
-        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
+        indexes = self.indexes[index * self.batch_size:(index + 1) * self.batch_size]
 
         # Generate data
         X, y = self.__data_generation(indexes)
@@ -172,7 +180,7 @@ class DataGenerator(keras.utils.Sequence):
             np.random.shuffle(self.indexes)
 
     def __data_generation(self, indexes):
-        'Generates data containing batch_size samples' # X : (n_samples, *dim, n_channels)
+        'Generates data containing batch_size samples'  # X : (n_samples, *dim, n_channels)
         # Initialization
         X = np.empty((self.batch_size, *self.dim, self.n_channels), dtype=np.float32)
         y = np.empty((self.batch_size, self.n_classes), dtype=int)
@@ -180,24 +188,25 @@ class DataGenerator(keras.utils.Sequence):
         # Generate data
         for batch_index, ds_index in enumerate(indexes):
             # Store sample
-            X[batch_index,:] = self.ds_imgs[ds_index]
+            X[batch_index, :] = self.ds_imgs[ds_index]
 
             # Store class
-            y[batch_index,:] = self.ds_labels[ds_index]
+            y[batch_index, :] = self.ds_labels[ds_index]
 
         return X, y
+
 
 """## History recording
 
 Callback class that records into a csv file the loss values and other performance measures at each training epoch
 """
 
+
 class LossHistory(keras.callbacks.Callback):
     def __init__(self, file_path, resume_training):
         super(LossHistory, self).__init__()
         self.file_path = file_path
         self.resume_training = resume_training
-
 
     def on_train_begin(self, logs={}):
         # if we are starting a new training, create the csv file and the header row
@@ -207,9 +216,10 @@ class LossHistory(keras.callbacks.Callback):
 
     def on_epoch_end(self, epoch, logs=None):
         with open(self.file_path, "a") as csv_file:
-            statistics = np.asarray([[logs.get("loss"), logs.get("accuracy"),\
-                                     logs.get("val_loss"), logs.get("val_accuracy")]])
+            statistics = np.asarray([[logs.get("loss"), logs.get("accuracy"), \
+                                      logs.get("val_loss"), logs.get("val_accuracy")]])
             np.savetxt(csv_file, statistics)
+
 
 """##Custom metrics"""
 
@@ -251,8 +261,9 @@ Use the VGG16 as base model for fine-tuning:
 # load VGG16, ensuring the head FC layer sets are left off, while at
 # the same time adjusting the size of the input image tensor to the
 # network
-baseModel = keras.applications.VGG16(weights=None, include_top=False,
-	input_tensor=Input(shape=(cnn_image_shape[0], cnn_image_shape[1], 3)), pooling="max")
+baseModel = keras.applications.VGG16(weights="imagenet", include_top=False,
+                                     input_tensor=Input(shape=(cnn_image_shape[0], cnn_image_shape[1], 3)),
+                                     pooling="max")
 
 """Add the custom structure (to be trained) on top of the (trained) convolutional part:"""
 
@@ -267,12 +278,12 @@ headModel = Dense(n_classes, activation="softmax")(headModel)
 cnn_model = Model(inputs=baseModel.input, outputs=headModel)
 # loop over all layers in the base model and freeze them so they will
 # *not* be updated during the first training process
-# for layer in baseModel.layers:
-# 	layer.trainable = False
+for layer in baseModel.layers:
+    layer.trainable = False
 
 """Compile the model basing on the configuration flags:"""
 
-opt = Adam(lr=0.005)
+opt = Adam(lr=learning_rate)
 
 # If we not want to train the network, load the already trained weights
 if not train_network:
@@ -295,14 +306,16 @@ logging.info(cnn_model.summary())
 Save the network model into a json file and its weights in a separate HDF5 file:
 """
 
+
 def save_cnn_to_file(cnn):
-  # serialize model to JSON
-  model_json = cnn.to_json()
-  with open(cnn_model_path, 'w') as json_file:
-    json_file.write(model_json)
-  # serialize weights to HDF5
-  cnn.save_weights(cnn_weights_path)
-  logging.info("Model saved")
+    # serialize model to JSON
+    model_json = cnn.to_json()
+    with open(cnn_model_path, 'w') as json_file:
+        json_file.write(model_json)
+    # serialize weights to HDF5
+    cnn.save_weights(cnn_weights_path)
+    logging.info("Model saved")
+
 
 """Train the network (if the corresponding flag is set) and save it into the Drive.
 
@@ -311,27 +324,28 @@ Save a model checkpoint after each improvement in accuracy with respect to the v
 
 # Data generators params
 data_gen_params = {'dim': (cnn_image_shape[0], cnn_image_shape[1]),
-        'batch_size': 32,
-        'n_classes': n_classes,
-        'n_channels': 3,
-        'shuffle': True}
+                   'batch_size': batch_size,
+                   'n_classes': n_classes,
+                   'n_channels': 3,
+                   'shuffle': True}
 
 if train_network:
     with h5py.File(cnn_dataset_path, 'r') as hdf_cnn_file:
         # Generators
         training_generator = DataGenerator(hdf_cnn_file["train"], hdf_cnn_file["train_labels"], **data_gen_params)
         validation_generator = DataGenerator(hdf_cnn_file["valid"], hdf_cnn_file["valid_labels"], **data_gen_params)
-        
-        checkpoint = ModelCheckpoint(cnn_weights_checkpoint_path)#, monitor="val_accuracy", mode="max", save_best_only=True)
+
+        checkpoint = ModelCheckpoint(
+            cnn_weights_checkpoint_path)  # , monitor="val_accuracy", mode="max", save_best_only=True)
         es = EarlyStopping(monitor="val_loss", mode="min", verbose=1, patience=100, restore_best_weights=True)
-        loss_history = LossHistory(file_path=history_stats_path,resume_training=resume_training)
+        loss_history = LossHistory(file_path=history_stats_path, resume_training=resume_training)
 
         # Train model on dataset
         history = cnn_model.fit_generator(generator=training_generator,
-                            validation_data=validation_generator,
-                            use_multiprocessing=True, epochs=1000,
-                            workers=0, callbacks=[es, checkpoint, loss_history], verbose=1)
-        
+                                          validation_data=validation_generator,
+                                          use_multiprocessing=True, epochs=1000,
+                                          workers=0, callbacks=[es, checkpoint, loss_history], verbose=1)
+
         logging.info("\nTraining completed")
 
     save_cnn_to_file(cnn_model)
@@ -357,7 +371,7 @@ with h5py.File(cnn_dataset_path, 'r') as hdf_cnn_file:
     logging.info(ds_labels_explicit.shape)
     logging.info(predicted_labels_explicit.shape)
     comparison_matrix = np.empty((ds_labels.shape[0], 2), dtype=np.int32)
-    comparison_matrix[:,0] = predicted_labels_explicit
-    comparison_matrix[:,1] = ds_labels_explicit
+    comparison_matrix[:, 0] = predicted_labels_explicit
+    comparison_matrix[:, 1] = ds_labels_explicit
     logging.info(comparison_matrix.shape)
     logging.info(comparison_matrix)
