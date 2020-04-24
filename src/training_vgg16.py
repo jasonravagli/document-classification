f7d6d7c801195788e973784a1ec4472aa93fd687
jasonravagli <jasonravagli@gmail.com>
2020-04-24 12:42:13 +0200

    Compatibility on accuracy metric with older keras version fixed

--- src/training_vgg16.py
+++ src/training_vgg16.py
@@ -216,7 +216,7 @@ class LossHistory(keras.callbacks.Callback):
 
     def on_epoch_end(self, epoch, logs=None):
         with open(self.file_path, "a") as csv_file:
-            statistics = np.asarray([[logs.get("loss"), logs.get("accuracy"), logs.get("val_loss"), logs.get("val_accuracy")]], dtype=np.float)
+            statistics = np.asarray([[logs.get("loss"), logs.get("acc"), logs.get("val_loss"), logs.get("val_acc")]], dtype=np.float)
             print("Saved statistics: " + str(statistics))
             np.savetxt(csv_file, statistics)
 
